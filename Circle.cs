43ada4c64626976687cf6ae14f811a76f0717640
Trần Như Sum <transum30052005@gmail.com>
2024-03-18 02:12:03 +0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿namespace bai2_07
+{
+    public class Circle
+    {
+        public double radius;
+        public string color;
+        public bool filled;
+
+        public Circle()
+        {
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle(double radius, string color, bool filled)
+        {
+            this.radius = radius;
+            this.color = color;
+            this.filled = filled;
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComperableCircle[] circles = new ComperableCircle[3];
+            circles[0] = new ComperableCircle(3.6);
+            circles[1] = new ComperableCircle();
+            circles[2] = new ComperableCircle(3.5, "indigo", false);
+
+            Console.WriteLine("Pre-sorted:");
+            foreach (ComperableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+
+            Array.Sort(circles);
+
+            Console.WriteLine("After-sorted:");
+            foreach (ComperableCircle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+            Console.ReadKey();
+        }
+    }
+    public class ComperableCircle : Circle, IComparable<ComperableCircle>
+    {
+
+        public ComperableCircle() { }
+        public ComperableCircle(double radius) : base(radius) { }
+        public ComperableCircle(double radius, string color, bool filled) : base(radius, color, filled) { }
+
+        public int CompareTo(ComperableCircle o)
+        {
+            if (getRadius() > o.getRadius()) return 1;
+            else if (getRadius() < o.getRadius()) return -1;
+            else return 1;
+        }
+    }
+}
